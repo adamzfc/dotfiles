a1f6b379a35c6e186251c5c03dcc79af747e649f
adamzfc <adamzfc@gmail.com>
2016-12-11 17:47:48 +0800

    sort out 2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s $(pwd -P)/bin ~/bin
+ln -s $(pwd -P)/vim ~/.vim
+ln -s $(pwd -P)/vimrc ~/.vimrc
+ln -s $(pwd -P)/zshrc ~/.zshrc
+ln -s $(pwd -P)/privoxy ~/.privoxy
+ln -s $(pwd -P)/aliasrc ~/.aliasrc
